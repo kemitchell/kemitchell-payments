985ca88872ea070a3385f6b68424d858bc49d6aa
Kyle Mitchell <kyle@kemitchell.com>
2015-04-20 01:42:56 +0000

    ping every thirty minutes on Heroku

--- server/serve.js
+++ server/serve.js
@@ -7,3 +7,13 @@ http.createServer(handler(
   process.env.STRIPE_PUBLISHABLE_KEY
 ))
   .listen(process.env.PORT || 8080);
+
+var APP_NAME = process.env.HEROKU_APP_NAME;
+if (APP_NAME) {
+  require('heroku-ping')({
+    apps: [{
+      name: APP_NAME,
+      secure: true
+    }]
+  });
+}
